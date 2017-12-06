4f8376db339eb0e79fe8c0f58074b5e225aadd6c
MJ <mariejoseblaise22@gmail.com>
2017-12-06 09:10:23 +0100

    change page de base

--- bienvenue-a-bord/sb-admin-react/src/routes/home/Home.js
+++ bienvenue-a-bord/sb-admin-react/src/routes/home/Home.js
@@ -60,15 +60,7 @@ function Home(props, context) {
           >
             <div>
               <ResponsiveContainer width="100%" aspect={2}>
-                <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }} >
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <CartesianGrid stroke="#ccc" />
-                  <Tooltip />
-                  <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
-                  <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-                  <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
-                </AreaChart>
+                
               </ResponsiveContainer>
             </div>
 
