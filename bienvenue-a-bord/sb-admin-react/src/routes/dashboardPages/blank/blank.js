7083c4e463065b5fba778750efbb57eeb6144536
Cindy Marques <CindyMarques@MacBook-Air-de-Cindy.local>
2017-12-06 17:20:53 +0100

    modifImgs

--- bienvenue-a-bord/sb-admin-react/src/routes/dashboardPages/blank/blank.js
+++ bienvenue-a-bord/sb-admin-react/src/routes/dashboardPages/blank/blank.js
@@ -1,5 +1,10 @@
 import React, { PropTypes } from 'react';
 import { PageHeader } from 'react-bootstrap';
+import lisbonne from '../../../images/lisbonne.jpg'
+import republiqueDominicaine from '../../../images/republiqueDominicaine.jpg'
+import thailande from '../../../images/thailande.jpg'
+import '../../../../../src/App.css'
+import history from '../../../core/history';
 
 const title = 'Blank';
 /* displayBlank */
@@ -10,6 +15,17 @@ function displayBlank(props, context) {
       <div className="row">
         <div className="col-lg-12">
           <PageHeader>Page des voyages !</PageHeader>
+        </div>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                  <td><img className="img-responsive" src={lisbonne} href="" onClick={(e) => { e.preventDefault(); history.push('/Voyage'); }}/></td>
+                  <td><img className="img-responsive" src={republiqueDominicaine}/></td>
+                  <td><img className="img-responsive" src={thailande}/></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
